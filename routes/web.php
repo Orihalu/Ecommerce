6310aa565509c0fa137c73ec7e8bfa074f9d72b5
vagrant <vagrant@localhost.localdomain>
2018-08-28 04:11:59 +0100

    set admin page and guards

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,16 @@ Route::get('/cart','UserController@showCart');
 Route::get('/checkout','CheckoutController@showCheckoutPage');
 
 Route::post('/home','ProductController@search');
+
+Route::group(['prefix' => 'admin'], function() {
+	Route::get('login', 'Admin\Auth\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\Auth\LoginController@login')->name('admin.login');
+	Route::get('logout', 'Admin\Auth\LoginController@logout')->name('admin.logout');
+
+	Route::get('register', 'Admin\Auth\RegisterController@showRegisterForm')->name('admin.register');
+	Route::post('register', 'Admin\Auth\RegisterController@register')->name('admin.register');
+
+	// Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+});
+
+// Route::get('/home', 'HomeController@index');
